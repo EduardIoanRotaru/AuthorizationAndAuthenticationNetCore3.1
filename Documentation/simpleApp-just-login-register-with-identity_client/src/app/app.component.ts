534aa684052bee1076b9910d93b13751b3c89ca2
Ioan Eduard <rotaru15@gmail.com>
2022-03-31 10:37:09 -0700

    clientAppsInitialized

---
+++ Documentation/simpleApp-just-login-register-with-identity_client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'simpleApp-just-login-register-with-identity_client';
+}
